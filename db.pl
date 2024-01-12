9f413d7e08991f68e971c6cdfecdb62594abb363
Enoch Kambale <campnoch@gmail.com>
2024-01-13 00:32:33 +0300

    predicates, atoms, facts, rules

---
+++ db.pl
@@ -0,0 +1,57 @@
+loves(romeo, juliet).
+
+loves(juliet, romeo) :- loves(romeo, juliet).
+
+male(albert).
+male(bob).
+male(bill).
+male(carl).
+male(charlie).
+male(dan).
+male(edward).
+
+female(alice).
+female(betsy).
+female(diana).
+
+happy(albert).
+happy(alice).
+happy(bob).
+happy(bill).
+with_albert(alice).
+
+runs(albert) :-
+    happy(albert).
+
+dances(alice) :-
+    happy(alice),
+    with_albert(alice).
+
+does_alice_dance :- dances(alice),
+    write('When Alice is happy and with Albert she dances').
+
+swims(bill) :-
+    happy(bill).
+
+swims(bill) :-
+    near_water(bill).
+
+
+
+
+parent(albert, bob).
+parent(albert, betsy).
+parent(albert, bill).
+
+parent(alice, bob).
+parent(alice, betsy).
+parent(alice, bill).
+
+parent(bob, carl).
+parent(bob, charlie).
+
+get_grandchild :-
+    parent(albert, X),
+    parent(X, Y),
+    write('Albert\'s grandchild is '),
+    write(Y), nl.
