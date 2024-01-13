364d773dc1a2fdc5f6a5e75af1f3439931fd8cbd
Enoch Kambale <campnoch@gmail.com>
2024-01-14 01:09:37 +0300

    looping with recursion

--- db.pl
+++ db.pl
@@ -174,3 +174,22 @@ process_stream(Char, Stream) :-
     write(Char),
     get_char(Stream, Char2),
     process_stream(Char2, Stream).
+
+
+% Loops - recursion
+count_to_10(10) :- write(10), nl.
+
+count_to_10(X) :-
+    write(X), nl,
+    Y is X + 1,
+    count_to_10(Y).
+
+count_down(Low, High) :-
+    between(Low, High, Y),
+    Z is High - Y,
+    write(Z), nl.
+
+count_up(Low, High) :-
+    between(Low, High, Y),
+    Z is Low + Y,
+    write(Z), nl.
